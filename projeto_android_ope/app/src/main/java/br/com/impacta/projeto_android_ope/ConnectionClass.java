2e9fd161315303bd9e24d6d77c7360680f81b359
Jefferson Verona <jefferson.verona@gmail.com>
2017-05-21 12:28:23 -0300

    Adicionei jtds-1.2.7.jar library, e criei classe para conexão

---
+++ projeto_android_ope/app/src/main/java/br/com/impacta/projeto_android_ope/ConnectionClass.java
@@ -0,0 +1,15 @@
+package br.com.impacta.projeto_android_ope;
+
+/**
+ * Created by jeffe on 21/05/2017.
+ */
+
+public class ConnectionClass {
+
+    String ip = "192.168.0.100";
+    String classs = "net.sourceforge.jtds.jdbc.Driver";
+    String db = "Andro";
+    String un = "hitesh";
+    String password = "789";
+
+}
